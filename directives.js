61acb6255a93e481fdb2c1cec0618f0244976b24
Paul Garcia <pbrain19@gmail.com>
2016-02-23 12:20:52 -0500

    directive started

---
+++ directives.js
@@ -0,0 +1,47 @@
+// <grid-container resource="/api/data.js">
+//     <grid-col name="Name" field="name"></grid-col>
+//     <grid-col name="Age" field="age"></grid-col>
+//     <grid-col name="Height" field="height"></grid-col>
+//   </grid-container>
+var app = angular.module('sample');
+
+
+app.directive('gridContainer', function() {
+  return {
+    restrict: 'E',
+    controller: function($scope, $http) {
+      $scope.cols = [];
+      $scope.rows = [];
+      this.addCol = function(col) {
+        $scope.cols.push(col);
+      }
+      $http.get('/api/data.json').success(function(response) {
+        console.log(response);
+        $scope.rows = response.data;
+      })
+
+    },
+    link: function(scope, el, attr) {
+      console.log('link for container')
+    },
+    transclude: true,
+    templateUrl: './autoComplete.html'
+  }
+});
+
+app.directive('gridCol', function() {
+  return {
+    restrict: 'E',
+    require: '^gridContainer',
+    link: function(scope, el, attr, gridContainerCTRL) {
+      var data = {
+        name: attr.name,
+        field: attr.field
+      };
+
+      console.log(data);
+      gridContainerCTRL.addCol(data);
+
+    }
+  }
+})
